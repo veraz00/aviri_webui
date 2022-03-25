74870d0d079e926945a43ef21c98b0a695095f71
veraz00 <83570482+veraz00@users.noreply.github.com>
2022-03-25 14:29:02 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -4,4 +4,5 @@ from app import create_app
 
 if __name__ == '__main__':
     app = create_app('develop')
+    app.jinjia_env.cache = {} # if front is installed in crystalvu, remove the cache limit 
     app.run(debug = True)
