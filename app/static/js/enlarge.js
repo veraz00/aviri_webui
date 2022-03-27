1bc2fd8537dbcaef1a220ada950b6756eb80de86
veraz00 <83570482+veraz00@users.noreply.github.com>
2022-03-27 21:45:05 +0800

    Delete app/static/js directory

--- app/static/js/enlarge.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('.image').click(function(){
-         $(this).css('width', function(_ , cur){
-          return cur === '100px' ? '100%' : '100px'
-        });  // original width is 500px 
-    });
-});
